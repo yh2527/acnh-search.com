656bcfc79f4de70fd3ff883691aed8a954f2f3cf
yh2527 <110083173+yh2527@users.noreply.github.com>
2023-11-08 22:10:16 -0800

    static site setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: '_static',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
